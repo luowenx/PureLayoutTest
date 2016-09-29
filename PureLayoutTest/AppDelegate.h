f3f007c3b80a0d0b7e8c0e241c8c38245a2557b7
luowenx <1056084639@qq.com>
2016-09-29 20:55:24 +0800

    Initial Commit

---
+++ PureLayoutTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PureLayoutTest
+//
+//  Created by luowenx on 16/9/29.
+//  Copyright © 2016年 xtkj_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PureLayoutTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PureLayoutTest
+//
+//  Created by luowenx on 16/9/29.
+//  Copyright © 2016年 xtkj_ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
